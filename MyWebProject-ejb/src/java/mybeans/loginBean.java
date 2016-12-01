2cae0800e739c9745247e28272d48ec8ebc7a11c
Mario Worbs <mario.worbs@picturemaxx.com>
2016-12-01 23:05:02 +0100

    initial commit

---
+++ MyWebProject-ejb/src/java/mybeans/loginBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mybeans;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author mario.worbs
+ */
+@Stateless
+@LocalBean
+public class loginBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
